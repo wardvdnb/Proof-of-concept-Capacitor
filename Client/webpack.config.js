b41b39c80ae0d3d1f250077192a1c4685e2767b4
Ward <ward.vdb@live.be>
2022-05-22 23:33:57 +0200

    updates

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,7 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
